beffe1a3a7ea6495519327de670a58ae608ead4d
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2026-02-03 17:30:04 +0100

    Add movement and collision with walls

---
+++ Assets/Scripts/Head.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head : MonoBehaviour
+{
+    public string name;
+    [SerializeField] float movmentSpeed = 1f;
+    [SerializeField] float turnSpeed = 180f;
+
+    public KeyCode LeftKey;
+    public KeyCode RightKey;
+
+    float input = 0;
+
+    public bool isAlive { get; private set; } = true;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!isAlive) return;
+            
+        input = getInput();
+    }
+
+    void FixedUpdate()
+    {
+        if(!isAlive) return;
+        
+        transform.Rotate(Vector3.forward * turnSpeed * -input * Time.fixedDeltaTime, Space.Self);
+        transform.Translate(Vector3.up * movmentSpeed * Time.fixedDeltaTime, Space.Self);
+    }
+
+    float getInput()
+    {
+        if (Input.GetKey(LeftKey)) return -1f;
+        else if(Input.GetKey(RightKey)) return 1f;
+        return 0f;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isAlive) return;
+
+        isAlive = false;
+        Debug.Log($"{name} uderzy³ w œciane!");
+    }
+}
