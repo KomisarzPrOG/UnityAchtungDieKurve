94cfb2dd9004416b827bd741ba8ee6e7a2a7985c
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2026-02-04 20:06:26 +0100

    Add SceneHandler to handle restarting the rounds

---
+++ Assets/Scripts/GameManager/SceneHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public static SceneHandler Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // by PrzemekBarczyk
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void NextScene()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(++currentIndex);
+    }
+}
