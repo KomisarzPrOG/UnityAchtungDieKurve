75bd1f6d150ab24785ed9e7ce7b05e76c7f61f06
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2026-02-07 21:09:52 +0100

    Add working player selection
    Which stores what players have been selected and what inputs for controls they chose

---
+++ Assets/Scripts/MainMenu/PlayerSelectManager.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerSelectManager : MonoBehaviour
+{
+    public static PlayerSelectManager Instance;
+
+    public List<PlayerConfig> selectedPlayers = new List<PlayerConfig>();
+    int currentPlayer = 0;
+    bool waitingForLeftKey = false;
+    bool waitingForRightKey = false;
+
+    HashSet<KeyCode> forbiddenKeys = new()
+    {
+        KeyCode.Escape,
+        KeyCode.Space,
+        KeyCode.F11,
+        KeyCode.Print,
+        KeyCode.LeftAlt,
+        KeyCode.RightAlt,
+        KeyCode.LeftControl,
+        KeyCode.RightControl,
+
+        KeyCode.Mouse0,
+        KeyCode.Mouse1,
+        KeyCode.Mouse2,
+        KeyCode.Mouse3,
+        KeyCode.Mouse4,
+        KeyCode.Mouse5,
+        KeyCode.Mouse6
+    };
+
+    void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public void SelectPlayer(int id, string name, Color color, TMP_Text left, TMP_Text right)
+    {
+        selectedPlayers.Add(new PlayerConfig(id, name, color, left, right));
+        if(selectedPlayers.Count > 0)
+            waitingForLeftKey = true;
+    }
+
+    private void Update()
+    {
+        if (waitingForLeftKey || waitingForRightKey)
+            DetectKey();
+
+        if (Input.GetKeyDown(KeyCode.Space) && selectedPlayers.Count >= 2)
+            StartGame();
+    }
+
+    void DetectKey()
+    {
+        foreach(KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if(!Input.GetKeyDown(key)) continue;
+            if(forbiddenKeys.Contains(key)) continue;
+
+            var p = selectedPlayers[currentPlayer];
+
+            if(p == null)
+            {
+                Debug.LogWarning("Nie ma takiego gracza");
+                break;
+            }
+
+            if(waitingForLeftKey)
+            {
+                p.leftKey = key;
+                waitingForLeftKey = false;
+                waitingForRightKey = true;
+                ChangeText("Left", p);
+                return;
+            }
+            
+            if(waitingForRightKey && p.leftKey != key)
+            {
+                p.rightKey = key;
+                waitingForRightKey = false;
+                ChangeText("Right", p);
+
+                currentPlayer++;
+                if(currentPlayer < selectedPlayers.Count)
+                    waitingForLeftKey = true;
+            }
+        }
+    }
+
+    void StartGame()
+    {
+        PlayerPrefsData.Save(selectedPlayers);
+        SceneHandler.Instance.NextScene();
+    }
+
+    void ChangeText(string control, PlayerConfig player)
+    {
+        if(control == "Left")
+            player.leftKeyText.text = player.leftKey.ToString();
+        else
+            player.rightKeyText.text = player.rightKey.ToString();
+    }
+}
