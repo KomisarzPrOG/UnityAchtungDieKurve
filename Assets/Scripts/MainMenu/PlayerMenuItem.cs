3c5f3726643e32b8c225a12c07382be10ebe6974
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2026-02-07 21:08:32 +0100

    Create PlayerMenuItem.cs
    Which handles what happens when player hovers on text and selects head

---
+++ Assets/Scripts/MainMenu/PlayerMenuItem.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Build;
+using UnityEngine;
+
+public class PlayerMenuItem : MonoBehaviour
+{
+    public TMP_Text[] texts;
+    public int playerID;
+
+    bool selected = false;
+
+    void SetAlpha(byte a)
+    {
+        Color c = texts[0].color;
+        c.a = a / 255f;
+
+        foreach(var t in texts)
+            t.color = c;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (!selected)
+            SetAlpha(128);
+    }
+
+    private void OnMouseExit()
+    {
+        if(!selected)
+            SetAlpha(64);
+    }
+
+    private void OnMouseDown()
+    {
+        if (selected)
+        {
+            SetAlpha(128);
+        }
+        else
+        {
+            PlayerSelectManager.Instance.SelectPlayer(playerID, texts[1].ToString(), texts[1].color, texts[2], texts[3]);
+            SetAlpha(255);
+        }
+
+        selected = !selected;
+    }
+}
