4823f10fcd1757fb7e8ae840eadab8dcb6af0cbe
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2026-02-11 20:01:38 +0100

    Add PlayerLoader.cs
    A script which upon loading to GameBoard scene generates PlayerPrefabs and sets their data from PlayerPrefsData.cs

---
+++ Assets/Scripts/Player/PlayerLoader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    private List<PlayerConfig> playerList = new List<PlayerConfig>();
+
+    void Start()
+    {
+        playerList = PlayerPrefsData.players;
+
+        LoadPlayers();
+    }
+
+    void LoadPlayers()
+    {
+        foreach(var player in playerList)
+        {
+            GameObject playerObject = Instantiate(playerPrefab, GameObject.Find("Players").transform);
+            Head playerHead = playerObject.GetComponentInChildren<Head>();
+            Tail playerTail = playerObject.GetComponentInChildren<Tail>();
+
+            playerHead.SetHead(player.playerName, player.playerID, player.leftKey, player.rightKey, player.playerMaterial.color);
+            playerTail.SetTail(player.playerMaterial);
+        }
+    }
+}
