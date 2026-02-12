d0aef4e6854abae01b3e5a4347b5ae639e4f5834
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2026-02-12 20:30:02 +0100

    Add SpeedBoost.cs
    PowerUp which upon collecting applies 1.25 multiplier to movement speed

---
+++ Assets/Scripts/PowerUps/Types/SpeedBoost.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoost : PowerUp
+{
+    public float speedMultiplier = 1.25f;
+    public float duration = 4f;
+
+    protected override void ApplySelf(Head player)
+    {
+        player.StartCoroutine(player.ModifySpeed(speedMultiplier, duration));
+    }
+
+    protected override void ApplyToOthers(Head collector)
+    {
+        List<Head> playerList = GameManager.Instance.GetAllPlayers();
+
+        foreach(Head player in playerList)
+        {
+            if(player != collector)
+                player.StartCoroutine(player.ModifySpeed(speedMultiplier, duration));
+        }
+    }
+
+    protected override void ApplyGlobal()
+    {
+        throw new System.NotImplementedException();
+    }
+}
