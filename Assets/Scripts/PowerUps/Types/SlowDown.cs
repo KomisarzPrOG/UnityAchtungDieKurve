ce4399ba0c28ee90e45f845b92b72810dad4154d
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2026-02-12 20:29:44 +0100

    Add SlowDown.cs
    PowerUp which upon collecting applies 0.75 multiplier to movement speed

---
+++ Assets/Scripts/PowerUps/Types/SlowDown.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDown : PowerUp
+{
+    public float speedMultiplier = 0.75f;
+    public float duration = 4f;
+
+    protected override void ApplySelf(Head player)
+    {
+        player.StartCoroutine(player.ModifySpeed(speedMultiplier, duration));
+    }
+
+    protected override void ApplyToOthers(Head collector)
+    {
+        List<Head> playerList = GameManager.Instance.GetAllPlayers();
+
+        foreach (Head player in playerList)
+        {
+            if (player != collector)
+                player.StartCoroutine(player.ModifySpeed(speedMultiplier, duration));
+        }
+    }
+
+    protected override void ApplyGlobal()
+    {
+        throw new System.NotImplementedException();
+    }
+}
