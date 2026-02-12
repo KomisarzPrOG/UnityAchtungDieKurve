92d6f43a31f700f1fbecb7729f720e1ad9b170c9
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2026-02-12 20:26:45 +0100

    Add abstract class as a template for PowerUps

---
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+public abstract class PowerUp : MonoBehaviour
+{
+    public List<PowerUpTargetType> possibleTargets = new List<PowerUpTargetType>();
+    public PowerUpTargetType targetType { get; private set; }
+    
+    public SpriteRenderer Border;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Head player = collision.gameObject.GetComponent<Head>();
+        
+        if (player == null)
+        {
+            Debug.LogWarning("PowerUp: Player is null");
+            return;
+        }
+
+        Activate(player);
+    }
+
+    public void Activate(Head collector)
+    {
+        switch (targetType)
+        {
+            case PowerUpTargetType.Self:
+                ApplySelf(collector);
+                break;
+            case PowerUpTargetType.Others:
+                ApplyToOthers(collector);
+                break;
+            case PowerUpTargetType.Global:
+                ApplyGlobal();
+                break;
+        }
+    }
+
+    protected abstract void ApplySelf(Head player);
+    protected abstract void ApplyToOthers(Head collector);
+    protected abstract void ApplyGlobal();
+
+    public void RandomizeTargetType()
+    {
+        if(possibleTargets == null || possibleTargets.Count == 0)
+            return;
+
+        int index = Random.Range(0, possibleTargets.Count);
+        targetType = possibleTargets[index];
+
+        SetPowerUp();
+    }
+
+    public void SetPowerUp()
+    {
+        switch(targetType)
+        {
+            case PowerUpTargetType.Self:
+                Border.color = Color.green;
+                break;
+            case PowerUpTargetType.Others:
+                Border.color = Color.red;
+                break;
+            case PowerUpTargetType.Global:
+                Border.color = Color.blue;
+                break;
+        }
+    }
+}
