d50145e2fbade5382ce2f7349115f722950abe9d
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2026-02-13 02:24:10 +0100

    Add ClearTail PowerUp
    Which clears all players tails with their hitboxes

---
+++ Assets/Scripts/PowerUps/Types/ClearTails.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearTails : PowerUp
+{
+    protected override void ApplySelf(Head player)
+    {
+        throw new System.NotImplementedException();
+    }
+    protected override void ApplyToOthers(Head collector)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void ApplyGlobal()
+    {
+        List<Head> playerList = GameManager.Instance.GetAllPlayers();
+
+        foreach (Head player in playerList)
+        {
+            if (player != null)
+            {
+                player.tail.ResetTail();
+            }
+        }
+    }
+}
