495949fa3f1b6ff105c6c5882f7ae05c834e115a
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2026-02-28 00:02:35 +0100

    Add SpawnPowerUps
    Which when collected spawns additional PowerUps

---
+++ Assets/Scripts/PowerUps/Types/SpawnPowerUps.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+public class SpawnPowerUps : PowerUp
+{
+    [Tooltip("Notice: this number is for some reason multiplied by 2")]
+    public int powerUpsCount = 2;
+
+    protected override void ApplySelf(Head player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void ApplyToOthers(Head collector)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void ApplyGlobal(Head collector)
+    {
+        for (int i = 0; i < powerUpsCount; i++)
+            PowerUpSpawner.Instance.SpawnPowerUp(true);
+    }
+}
