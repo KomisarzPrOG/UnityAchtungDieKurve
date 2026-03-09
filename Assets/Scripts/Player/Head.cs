2461c69da54ba90eabb92f51f794b7be5aa84f50
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2026-03-09 22:25:05 +0100

    Refactor Head.cs
    Refactor Head.cs to make it more readable and less junky

--- Assets/Scripts/Player/Head.cs
+++ Assets/Scripts/Player/Head.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Head : MonoBehaviour
@@ -7,59 +8,41 @@ public class Head : MonoBehaviour
     public string Name;
     public int id;
     public Tail tail;
-    [SerializeField] float baseSpeed = 1f;
-    [SerializeField] float turnSpeed = 180f;
-
-    public KeyCode LeftKey;
-    public KeyCode RightKey;
-
-    private int reverseEffectCount = 0;
-    private KeyCode originalLeftKey;
-    private KeyCode originalRightKey;
-
-    private Border border;
-
-    float input = 0;
-
-    public bool isAlive = true;
     public Color playerColor;
 
-    private List<float> speedModifiers = new List<float>();
-    float currentSpeed
-    {
-        get
-        {
-            float speed = baseSpeed;
-
-            foreach (float modifier in speedModifiers)
-                speed *= modifier;
-
-            return speed;
-        }
-    }
-
-
+    [SerializeField] float baseSpeed = 1f;
+    [SerializeField] float turnSpeed = 180f;
     [SerializeField] float blinkSpeed = 2f;
 
+    public KeyCode LeftKey { get; private set; }
+    public KeyCode RightKey { get; private set; }
+    private KeyCode originalLeftKey, originalRightKey;
+
+    private Border border;
     private SpriteRenderer spriteRenderer;
-    private Coroutine blinkRoutine;
+    private Coroutine warpBlinkRoutine;
 
+    float input = 0;
+    public bool isAlive = true;
 
-    public bool phaseWalk { get; private set; } = false;
-    int phaseWalkCount = 0;
+    private Dictionary<string, int> effectCounts = new();
 
-    public bool mazeMove { get; private set; } = false;
-    int mazeMoveCount = 0;
+    public bool phaseWalk => GetEffect("phase") > 0;
+    public bool mazeMove => GetEffect("maze") > 0;
+    public bool playerWrap => GetEffect("wrap") > 0;
 
-
-    private int sizeEffectCount = 0;
     public float sizeMultiplier { get; private set; } = 1f;
     private Vector3 baseScale;
     public float CurrentSize => sizeMultiplier;
 
-    public bool playerWrap { get; private set; } = false;
-    private int playerWrapCount = 0;
-    private Coroutine warpBlinkRoutine;
+    private List<float> speedModifiers = new();
+    float currentSpeed => speedModifiers.Count == 0
+        ? baseSpeed
+        : baseSpeed * speedModifiers.Aggregate(1f, (acc, m) => acc * m);
+
+    /* ========================
+            UNITY LIFECYCLE
+       ======================== */
 
     void Awake()
     {
@@ -67,56 +50,49 @@ public class Head : MonoBehaviour
         border = GameObject.Find("GameBorder").GetComponent<Border>();
         baseScale = transform.localScale;
 
-        var settings = GameSettings.Instance;
-        baseSpeed = settings.playerSpeed;
-        turnSpeed = settings.playerTurnSpeed;
+        var s = GameSettings.Instance;
+        baseSpeed = s.playerSpeed;
+        turnSpeed = s.playerTurnSpeed;
     }
 
-    void Start()
-    {
-        GameManager.Instance.RegisterPlayer(this);
-    }
+    void Start() => GameManager.Instance.RegisterPlayer(this);
 
     void Update()
     {
         if (!isAlive) return;
-
-        if (mazeMove)
-            MazeMoveHandler();
-        else
-            input = getInput();
+        input = mazeMove ? 0 : GetInput();
+        if (mazeMove) HandleMazeInput();
     }
 
     void FixedUpdate()
     {
-        if(!isAlive) return;
-
+        if (!isAlive) return;
         border.PlayerOffScreen(this);
-
-        if(!mazeMove)
+        if (!mazeMove)
             transform.Rotate(Vector3.forward * turnSpeed * -input * Time.fixedDeltaTime, Space.Self);
-        
         transform.Translate(Vector3.up * currentSpeed * Time.fixedDeltaTime, Space.Self);
     }
 
-    void MazeMoveHandler()
-    {
-        if (Input.GetKeyDown(LeftKey))
-        {
-            transform.Rotate(Vector3.forward * 90f);
-        }
-        else if (Input.GetKeyDown(RightKey))
-        {
-            transform.Rotate(Vector3.forward * -90f);
-        }
-    }
+    /* ========================
+              INPUT
+       ======================== */
 
-    float getInput()
+    float GetInput()
     {
         if (Input.GetKey(LeftKey)) return -1f;
-        else if(Input.GetKey(RightKey)) return 1f;
+        if (Input.GetKey(RightKey)) return 1f;
         return 0f;
     }
+
+    void HandleMazeInput()
+    {
+        if (Input.GetKeyDown(LeftKey)) transform.Rotate(Vector3.forward * 90f);
+        else if (Input.GetKeyDown(RightKey)) transform.Rotate(Vector3.forward * -90f);
+    }
+
+    /* ========================
+            DEATH & COLLISION
+       ======================== */
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -124,66 +100,49 @@ public class Head : MonoBehaviour
         if (collision.CompareTag("Head")) return;
 
         PowerUp powerUp = collision.GetComponent<PowerUp>();
-        if (powerUp != null)
-        {
-            powerUp.Activate(this);
-            Destroy(powerUp.gameObject);
-            return;
-        }
+        if (powerUp != null) { powerUp.Activate(this); Destroy(powerUp.gameObject); return; }
 
-        if (phaseWalk && collision.CompareTag("Tail"))
-            return;
+        if (phaseWalk && collision.CompareTag("Tail")) return;
+        if ((border.wrappedActive || playerWrap) && collision.CompareTag("Border")) return;
 
-        if((border.wrappedActive || playerWrap) && collision.CompareTag("Border"))
-            return;
-
-        if (collision.CompareTag("Tail"))
-        {
-            Tail hitTail = collision.GetComponentInParent<Tail>();
-
-            if (hitTail != null && hitTail.Owner == this)
-            {
-                Vector2 contactPoint = collision.ClosestPoint(transform.position);
-
-                Vector2 toContact = (contactPoint - (Vector2)transform.position).normalized;
-                Vector2 forward = transform.up;
-
-                float dot = Vector2.Dot(forward, toContact);
-
-                if (dot < 0f)
-                    return;
-            }
-        }
+        if (collision.CompareTag("Tail") && IsHittingOwnTailFromBehind(collision)) return;
 
         PlayerDeath();
+    }
+
+    bool IsHittingOwnTailFromBehind(Collider2D collision)
+    {
+        Tail hitTail = collision.GetComponentInParent<Tail>();
+        if (hitTail == null || hitTail.Owner != this) return false;
+
+        Vector2 toContact = ((Vector2)collision.ClosestPoint(transform.position)
+                             - (Vector2)transform.position).normalized;
+        return Vector2.Dot(transform.up, toContact) < 0f;
     }
 
     public void PlayerDeath(string customMessage = "")
     {
         isAlive = false;
-        gameObject.GetComponent<SpriteRenderer>().color = Color.gray;
-
+        spriteRenderer.color = Color.gray;
         GameManager.Instance.CheckPlayers();
         ScoreHandler.Instance.OnPlayerDeath(id);
-        
         Debug.Log(customMessage != "" ? customMessage : $"{Name} hit a wall!");
     }
 
+    /* ========================
+            SETUP
+       ======================== */
+
     public void SetHead(string name, int newId, KeyCode leftKey, KeyCode rightKey, Color color)
     {
-        Name = name;
-        id = newId;
-        LeftKey = leftKey;
-        RightKey = rightKey;
-        playerColor = color;
-
-        originalLeftKey = leftKey;
-        originalRightKey = rightKey;
+        Name = name; id = newId; playerColor = color;
+        LeftKey = originalLeftKey = leftKey;
+        RightKey = originalRightKey = rightKey;
     }
 
-    /* ====================
-          POWERUP METHODS
-       ==================== */
+    /* ========================
+            POWER-UPS
+       ======================== */
 
     public IEnumerator ModifySpeed(float multiplier, float duration)
     {
@@ -194,115 +153,91 @@ public class Head : MonoBehaviour
 
     public IEnumerator ReverseKeyBinds(float duration)
     {
-        reverseEffectCount++;
-
-        if(reverseEffectCount == 1)
+        if (AddEffect("reverse") == 1)
         {
-            LeftKey = originalRightKey;
-            RightKey = originalLeftKey;
+            (LeftKey, RightKey) = (originalRightKey, originalLeftKey);
             spriteRenderer.color = Color.blue;
         }
-
         yield return new WaitForSeconds(duration);
-
-        reverseEffectCount--;
-
-        if(reverseEffectCount == 0)
+        if (RemoveEffect("reverse") == 0)
         {
-            LeftKey = originalLeftKey;
-            RightKey = originalRightKey;
+            (LeftKey, RightKey) = (originalLeftKey, originalRightKey);
             spriteRenderer.color = Color.yellow;
         }
     }
 
     public IEnumerator ActivatePhaseWalk(float duration)
     {
-        phaseWalkCount++;
-
-        if(phaseWalkCount == 1)
-            phaseWalk = true;
-
+        AddEffect("phase");
         yield return new WaitForSeconds(duration);
-
-        phaseWalkCount--;
-
-        if(phaseWalkCount == 0)
-        {
-            tail.StartNewSegment();
-
-            phaseWalk = false;
-        }
+        if (RemoveEffect("phase") == 0) tail.StartNewSegment();
     }
 
     public IEnumerator ActivateMazeMove(float duration)
     {
-        mazeMoveCount++; 
-
-        if(mazeMoveCount == 1)
-            mazeMove = true;
-
+        AddEffect("maze");
         yield return new WaitForSeconds(duration);
-
-        mazeMoveCount--;
-
-        if(mazeMoveCount == 0)
-            mazeMove = false;
+        RemoveEffect("maze");
     }
 
     public IEnumerator ModifySize(float multiplier, float duration)
     {
-        sizeEffectCount++;
-
         sizeMultiplier *= multiplier;
         ApplySize();
-
+        AddEffect("size");
         yield return new WaitForSeconds(duration);
-
         sizeMultiplier /= multiplier;
-        sizeEffectCount--;
-
+        RemoveEffect("size");
         ApplySize();
-    }
-
-    void ApplySize()
-    {
-        transform.localScale = baseScale * sizeMultiplier;
-
-        tail.StartNewSegment();
-        tail.SetSize(sizeMultiplier);
     }
 
     public IEnumerator PlayerWarp(float duration)
     {
-        playerWrapCount++;
-
-        if(playerWrapCount == 1)
-        {
-            playerWrap = true;
+        if (AddEffect("wrap") == 1)
             warpBlinkRoutine = StartCoroutine(WarpBlink());
-        }
-
         yield return new WaitForSeconds(duration);
-
-        playerWrapCount--;
-
-        if(playerWrapCount == 0)
+        if (RemoveEffect("wrap") == 0)
         {
-            playerWrap = false;
-            if(warpBlinkRoutine != null)
-                StopCoroutine(warpBlinkRoutine);
+            if (warpBlinkRoutine != null) StopCoroutine(warpBlinkRoutine);
         }
+    }
+
+    /* ========================
+            HELPERS
+       ======================== */
+
+    void ApplySize()
+    {
+        transform.localScale = baseScale * sizeMultiplier;
+        tail.StartNewSegment();
+        tail.SetSize(sizeMultiplier);
+    }
+
+    int AddEffect(string key)
+    {
+        effectCounts.TryGetValue(key, out int count);
+        effectCounts[key] = ++count;
+        return count;
+    }
+
+    int RemoveEffect(string key)
+    {
+        effectCounts.TryGetValue(key, out int count);
+        effectCounts[key] = --count;
+        return count;
+    }
+
+    int GetEffect(string key)
+    {
+        effectCounts.TryGetValue(key, out int count);
+        return count;
     }
 
     private IEnumerator WarpBlink()
     {
         while (true)
         {
-            float alpha = Mathf.Lerp(0.2f, 1f,
-                Mathf.PingPong(Time.time * blinkSpeed, 1f));
-
-            SetAlpha(alpha);
-
+            SetAlpha(Mathf.Lerp(0.2f, 1f, Mathf.PingPong(Time.time * blinkSpeed, 1f)));
             yield return null;
         }
     }
